089ad1b6a1c08fd470e8aa5040e40b102f0586e8
Joshua Meadows <jtmeadows@mail.wvu.edu>
2024-01-26 13:35:50 -0500

    Add project files.

---
+++ Homework1Meadows/Pages/SearchResults.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Homework1Meadows.Pages
+{
+    public class SearchResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
